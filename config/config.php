24c28b8611e93557601766569e4f59eb21cc418b
neowei <447235366@qq.com>
2016-03-01 16:32:53 +0800

    .

--- config/config.php
+++ config/config.php
@@ -6,9 +6,9 @@ if(!defined('PIGCMS_PATH')) exit('deny access!');
 
 return array(
 	'DB_HOST'=>'localhost',
-	'DB_NAME'=>'newdian',
+	'DB_NAME'=>'pigcms',
 	'DB_USER'=>'root',
-	'DB_PWD'=>'198711',
+	'DB_PWD'=>'neowei',
 	'DB_PORT'=>'3306',
 	'DB_PREFIX'=>'pigcms_',
 	'STATIC_RESOURCE'=>'1'
